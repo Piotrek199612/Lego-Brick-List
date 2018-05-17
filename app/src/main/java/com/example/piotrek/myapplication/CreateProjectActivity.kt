1f18c39124b91f754cf721170fd9c94f265cd148
Piotrek199612 <a21sturmer@op.pl>
2018-05-17 10:45:24 +0200

    Files added.

--- app/src/main/java/com/example/piotrek/myapplication/CreateProjectActivity.kt
+++ app/src/main/java/com/example/piotrek/myapplication/CreateProjectActivity.kt
@@ -1,2 +1,163 @@
 package com.example.piotrek.myapplication
 
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_start.*
+import android.util.Log
+import java.sql.Array
+import android.view.ViewGroup
+import kotlin.math.log
+import android.R.interpolator.linear
+import android.os.StrictMode
+import android.view.Gravity
+import kotlinx.android.synthetic.main.activity_add_project.*
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
+import java.net.URL
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import android.util.Xml
+import android.widget.*
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
+
+class CreateProjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_project)
+        Log.i("TEST","SECOND")
+    }
+    fun createProject(view:View)
+    {
+        var name = nameEdit.text.toString()
+        createInventory(name)
+
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().permitAll().build())
+
+        var url = URL("http://fcds.cs.put.poznan.pl/MyWeb/BL/" + idEdit.text.toString() +".xml")
+        var urlconn= url.openConnection()
+        urlconn.connect()
+        var instream = urlconn.getInputStream()
+
+
+        var items = parseInventory(instream)
+
+        var db = DataBaseHelper(this)
+        db.openDataBase()
+        for (i in items)
+        {
+            if (i.size != 8)
+            {
+                throw Exception("Bad XML parsing")
+            }
+            if (i[5] == "N")
+            {
+                var cursor = db.readableDatabase.query("Inventories" ,
+                        arrayOf("_id"),
+                        "Name == ?", arrayOf(name),
+                        null, null, null)
+                cursor.moveToFirst()
+                var InventoryID = cursor.getString(cursor.getColumnIndex("_id"))
+                cursor.close()
+
+
+                var itemtype = i[0]
+                cursor = db.readableDatabase.query("ItemTypes" ,
+                        arrayOf("_id"),
+                        "Code == ?", arrayOf(itemtype),
+                        null, null, null)
+
+                cursor.moveToFirst()
+                var TypeID = cursor.getString(cursor.getColumnIndex("_id")).toInt()
+                cursor.close()
+
+
+                var color = i[3]
+                cursor = db.readableDatabase.query("Colors" , arrayOf("_id"), "Code == ?", arrayOf(color), null, null, null)
+                cursor.moveToFirst()
+                var ColorID = cursor.getString(cursor.getColumnIndex("_id")).toInt()
+                cursor.close()
+
+
+                var itemId = i[1]
+                cursor = db.readableDatabase.query("Parts" , arrayOf("_id"), "Code == ?", arrayOf(itemId), null, null, null)
+                cursor.moveToFirst()
+                if (cursor.count == 0)
+                {
+                    var toast = Toast.makeText(this,
+                            "Part with number $itemId and color $color doesnt exist in Database",
+                             Toast.LENGTH_LONG)
+                    toast.show()
+                    continue
+                }
+                var ItemID = cursor.getString(cursor.getColumnIndex("_id")).toInt()
+                cursor.close()
+
+                var Extra = i[4]
+                var Quantity = i[2].toInt()
+
+                val values = ContentValues()
+                values.put("InventoryID", InventoryID)
+                values.put("TypeID", TypeID)
+                values.put("ItemID", ItemID)
+                values.put("QuantityInSet", Quantity)
+                values.put("QuantityInStore", 0)
+                values.put("ColorID", ColorID)
+                values.put("Extra", Extra)
+                db.writableDatabase.insert("InventoriesParts", null, values)
+            }
+        }
+        db.close()
+        finish()
+    }
+
+    private fun createInventory(name:String)
+    {
+        var db = DataBaseHelper(this)
+        db.openDataBase()
+        val values = ContentValues()
+        values.put("Name", name)
+        values.put("Active", 1)
+        values.put("LastAccessed", 0)
+        db.writableDatabase.insert("Inventories", null, values)
+//TODO Check if inserted name must be unique
+        db.close()
+    }
+
+    private fun parseInventory(data: InputStream): ArrayList<ArrayList<String>> {
+        val factory = XmlPullParserFactory.newInstance()
+        factory.isNamespaceAware = true
+        val xpp = factory.newPullParser()
+
+        xpp.setInput(InputStreamReader(data))
+
+        var items = ArrayList<ArrayList<String>>()
+        var started = false
+
+        var tmp: ArrayList<String> = ArrayList()
+
+        while (xpp.eventType != XmlPullParser.END_DOCUMENT) {
+            if (xpp.eventType == XmlPullParser.START_TAG && xpp.name == "ITEM") {
+                    started = true
+            }
+            else if(xpp.eventType == XmlPullParser.END_TAG && xpp.name == "ITEM" && started) {
+                    items.add(tmp.clone() as ArrayList<String>)
+                    tmp.clear()
+                    started = false
+            } else if(xpp.eventType == XmlPullParser.TEXT) {
+                if (started && !xpp.text.contains("\n"))
+                    tmp.add(xpp.text)
+            }
+            xpp.next()
+        }
+        return items
+    }
+}
--- app/src/main/java/com/example/piotrek/myapplication/StartActivity.kt
+++ app/src/main/java/com/example/piotrek/myapplication/StartActivity.kt
@@ -1,15 +1,34 @@
 package com.example.piotrek.myapplication
 
+import android.content.ContentValues
+import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_start.*
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.view.Gravity
+import kotlinx.android.synthetic.main.activity_add_project.*
+import java.net.HttpURLConnection
+import java.net.URL
+import android.os.StrictMode
+
+
+
 
 class StartActivity : AppCompatActivity() {
 
     private val RED: Int = Color.rgb(211,50,50)
     private val GREEN: Int = Color.rgb(50,150,50)
+
+    private var inventories: Cursor? = null
+    private lateinit var db : DataBaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,20 +39,91 @@ class StartActivity : AppCompatActivity() {
 
         archiveButton.setBackgroundColor(Color.CYAN)
         addProjectButton.setBackgroundColor(Color.CYAN)
+
+        db = DataBaseHelper(this)
+        db.createDataBase()
+        db.openDataBase()
+
+        for (i :Int in 1..10)
+        {
+            val values = ContentValues()
+            values.put("Name", "Project Name$i")
+            values.put("Active", i%2)
+            values.put("LastAccessed", i%4)
+            db.writableDatabase.insert("Inventories", null, values)
+        }
+        db.close()
+        getData()
+
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        getData()
+    }
+
+    fun getData()
+    {
+        db = DataBaseHelper(this)
+        db.openDataBase()
+
+        inventoryLayout.removeAllViews()
+
+        var archive : String? = null
+        if (archivalButton.text == getString(R.string.archival_button_off))
+            archive = "Active <> 0"
+
+        var cursor = db.readableDatabase.query("Inventories" , arrayOf("Name, Active, LastAccessed"), archive, null, null, null, "LastAccessed ASC")
+
+        if (cursor.moveToFirst()) {
+            do {
+                var name = cursor.getString(cursor.getColumnIndex("Name"))
+                    var layout = LinearLayout(this)
+                    layout.orientation = LinearLayout.HORIZONTAL
+                    layout.layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT)
+
+                    var nameLabel = TextView(this)
+                    nameLabel.text = name
+                    nameLabel.gravity = Gravity.CENTER
+                    nameLabel.layoutParams = LinearLayout.LayoutParams(0,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 0.5f);
+
+
+                    var but1 = Button(this)
+                    but1.text = "DETAILS"
+                    but1.layoutParams = LinearLayout.LayoutParams(0,
+                            LinearLayout.LayoutParams.WRAP_CONTENT, 0.5f);
+
+                    layout.addView(nameLabel)
+                    layout.addView(but1)
+                    inventoryLayout.addView(layout)
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+    }
     fun archivalSwitch(view: View)
     {
         if (archivalButton.text == getString(R.string.archival_button_off))
         {
             archivalButton.text = getString(R.string.archival_button_on)
             archivalButton.setBackgroundColor(GREEN)
-
+            getData()
         }
         else
         {
             archivalButton.text = getString(R.string.archival_button_off)
             archivalButton.setBackgroundColor(RED)
+            getData()
         }
+    }
+
+    fun addProject(view:View)
+    {
+        Log.i("TEST","DZIALA")
+        val intent = Intent(this, CreateProjectActivity::class.java)
+        startActivity(intent)
     }
 }
